38795683a09599db62ddc13596661123cf7a83d5
Bfalconer24 <braxfalconer@gmail.com>
2026-07-21 01:28:34 -0500

    Initial API data fetching script

---
+++ api_fetch.py
@@ -0,0 +1,24 @@
+import requests
+
+print("Sports Dashboard Started")
+
+url = "https://www.thesportsdb.com/api/v1/json/123/searchteams.php?t=Arsenal"
+
+print(url)
+
+response = requests.get(url)
+print(response.status_code)
+
+data = response.json()
+
+print(data)
+
+if data["teams"] is None:
+    print("No teams found.")
+else:
+    for team in data["teams"]:
+        print(team["strTeam"])
+        print(team["strLeague"])
+        print(team["strStadium"])
+        print(team["strCountry"])
+        print(team["intFormedYear"])
